ab89e25ebfd308d2e6e4c511ce4389a0090a27a8
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-06-02 00:25:38 +0600

    Updates

---
+++ precache-manifest.4bbd3e6e0ec6de995216f54c9d249b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec462d7c00379b77e88fe9d943ccc6ba",
+    "url": "/React-Poll-App/index.html"
+  },
+  {
+    "revision": "385c4dc132a927bac680",
+    "url": "/React-Poll-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "385c4dc132a927bac680",
+    "url": "/React-Poll-App/static/js/2.2aec2282.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Poll-App/static/js/2.2aec2282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9711fe54344f8a3f7b",
+    "url": "/React-Poll-App/static/js/main.530fed34.chunk.js"
+  },
+  {
+    "revision": "115951ffa9340e4ca7e4",
+    "url": "/React-Poll-App/static/js/runtime-main.395bc172.js"
+  }
+]);
